adf6f3728c15a2f51def361d5b2cae448e01c43b
dpolevodin <dpolevodin@gmail.com>
2021-10-26 19:32:17 +0500

    added main-apge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
-// import './App.css';
+import '../src/css/reset.css';
+import '../src/css/style.css';
 
 function App() {
   return (
-    'test'
+    <body class="body">
+      <div class="main-wrapper">
+        <div class="page-header">
+            <h1 class="page-header__content">
+                Page header
+            </h1>
+        </div>
+        <div class="filter">
+            <div class="filter__search">
+                <div class="filter__search-field"></div>
+                <div class="filter__search-options"></div>
+            </div>
+            <div class="filter__button"></div>
+        </div>
+        <div class="table">
+            <div class="table__header"></div>
+            <div class="table__body"></div>
+            <div class="table__footer"></div>
+        </div>
+    </div>
+    </body>
   );
 }
 
