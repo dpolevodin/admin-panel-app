57bb254d6b7c308e620a29ea1e0e9bcec8b2c9b1
dpolevodin <dpolevodin@gmail.com>
2021-11-18 19:19:41 +0500

    added footer to form

--- src/components/ModalForm/ModalForm.js
+++ src/components/ModalForm/ModalForm.js
@@ -1,23 +1,37 @@
-import modalForm from "./css/ModalForm.module.css"
-import Button from "../Common/Button"
+import modalForm from "./css/ModalForm.module.css";
+import Button from "../Common/Button";
 
-const inputIconName = "incorrect"
+const headerIconName = "incorrect";
+const footerIconName = "checkmark";
+const buttonText = 'Сохранить'
 
 const ModalForm = () => {
+  return (
+    <div className={modalForm._}>
+      <div className={modalForm.form}>
+        <header className={modalForm.header}>
+          <div className={modalForm.header__title}>Заявка #2353474</div>
+          <Button
+            className={modalForm.button}
+            svgName={headerIconName}
+            iconClassName={modalForm.header__icon}
+          />
+        </header>
+        <div className={modalForm.table}></div>
+        <footer className={modalForm.footer}>
+          <div className={modalForm.footer__text}>
+            Ошибка или индикатор загрузки
+          </div>
+          <Button
+            className={modalForm.footer__button}
+            svgName={footerIconName}
+            buttonText={buttonText}
+            iconClassName={modalForm.footer__icon}
+          />
+        </footer>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className={modalForm._}>
-            <div className={modalForm.form}>
-                <header className={modalForm.header}>
-                    <div className={modalForm.header__title}>Заявка #2353474</div>
-                    <Button className={modalForm.button} svgName={inputIconName} iconClassName={modalForm.header__icon}/>
-                </header>
-                <div className={modalForm.table}>
-                </div>
-                <footer className={modalForm.footer}></footer>
-            </div>
-        </div>
-    )
-}
-
-export default ModalForm
+export default ModalForm;
