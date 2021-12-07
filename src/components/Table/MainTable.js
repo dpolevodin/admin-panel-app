003b406d2353e4e1147ec0eacf7f04422aac8bf9
dpolevodin <dpolevodin@gmail.com>
2021-12-07 19:20:50 +0500

    final commit. Version to review in main brach

--- src/components/Table/MainTable.js
+++ src/components/Table/MainTable.js
@@ -12,17 +12,17 @@ export const MainTable = () => {
 
   const handleButtonTheme = (event) => {
     dispatch(themeActions.changeTheme());
-    const mainBackground = isDarkTheme ? "#FFF" : "#2B2D33";
+    const mainBackgroundColor = isDarkTheme ? "#FFF" : "#2B2D33";
     const headerColor = isDarkTheme ? "#000" : "#BCC4CC";
     const inputColor = isDarkTheme ? "#FFF" : "#2B2D33";
-    const dayLight = isDarkTheme ? "#ebf0f5" : "#1F2629";
-    const light = isDarkTheme ? "#bad8f5" : "#454E52";
+    const dayLightColor = isDarkTheme ? "#ebf0f5" : "#1F2629";
+    const lightColor = isDarkTheme ? "#bad8f5" : "#454E52";
     const style = document.body.style;
-    style.setProperty("--main-background", mainBackground);
+    style.setProperty("--main-background", mainBackgroundColor);
     style.setProperty("--background-admin-panel-black", headerColor);
     style.setProperty("--background-admin-panel-white", inputColor);
-    style.setProperty("--background-admin-panel-day-light", dayLight);
-    style.setProperty("--background-admin-panel-light", light);
+    style.setProperty("--background-admin-panel-day-light", dayLightColor);
+    style.setProperty("--background-admin-panel-light", lightColor);
   };
   return (
     <div className="main-wrapper">
